65762b5e737baceb40a1d981e3e88b03c9f255a6
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-05-03 10:14:18 +0700

    (React Rendering) : Nested Lists

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,28 +1,44 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "1.000.000",
+    images: "/images/shoes1.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam et quisquam aperiam, harum voluptate aliquam architecto sequi voluptas, vitae blanditiis excepturi necessitatibus explicabo. At quisquam quibusdam hic, veritatis rem delectus!",
+  },
+  {
+    id: 2,
+    name: "Baju Keren",
+    price: "500.000",
+    images: "/images/clothes1.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam et quisquam aperiam, harum voluptate aliquam architecto sequi voluptas, vitae blanditiis excepturi necessitatibus explicabo. At quisquam quibusdam hic, veritatis rem delectus!",
+  },
+  {
+    id: 3,
+    name: "Tas Trendy",
+    price: "800.000",
+    images: "/images/bag1.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam et quisquam aperiam, harum voluptate aliquam architecto sequi voluptas, vitae blanditiis excepturi necessitatibus explicabo. At quisquam quibusdam hic, veritatis rem delectus!",
+  },
+];
+
 const ProductPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam et
-          quisquam aperiam, harum voluptate aliquam architecto sequi voluptas,
-          vitae blanditiis excepturi necessitatibus explicabo. At quisquam
-          quibusdam hic, veritatis rem delectus!
-        </CardProduct.Body>
-        <CardProduct.Footer price="1.000.000" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam et
-          quisquam aperiam, harum voluptate aliquam architecto sequi voluptas,
-          vitae blanditiis excepturi necessitatibus explicabo. At quisquam
-          quibusdam hic, veritatis rem delectus!
-        </CardProduct.Body>
-        <CardProduct.Footer price="1.000.000" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.images} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
