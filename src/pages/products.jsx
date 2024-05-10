fe47212532b242b1b62d9cab87db03b494dfc85c
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-05-10 16:44:40 +0700

    (React useContext) : useContext

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import { getProducts } from "../services/product.services";
 import { useLogin } from "../hooks/useLogin";
 import TableCart from "../components/Fragments/TableCart";
 import Navbar from "../components/Layouts/Navbar";
+import { DarkMode } from "../context/DarkMode";
 
 const ProductsPage = () => {
   const [products, setProducts] = useState([]);
+  const { isDarkMode } = useContext(DarkMode);
+
   useLogin();
 
   useEffect(() => {
@@ -21,7 +24,9 @@ const ProductsPage = () => {
       <Navbar />
       {/* =======END NAVBAR ======= */}
 
-      <div className="flex justify-center py-5">
+      <div
+        className={`flex justify-center py-5 ${isDarkMode && "bg-slate-900"}`}
+      >
         {/* ======= CARD PRODUCT =======  */}
         <div className="w-4/6 flex flex-wrap">
           {products.length > 0 &&
